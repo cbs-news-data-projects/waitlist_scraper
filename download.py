a85dede196995a3f59c68f5dae7912e0fd73f1f2
scottpham <scottgpham@gmail.com>
2024-05-20 15:10:32 -0400

    Initial commit

---
+++ download.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import requests
+import glob
+
+url = "https://data.sfgov.org/api/views/w4sk-nq57/rows.csv?accessType=DOWNLOAD"
+
+
+
+def check_date(today):
+
+    existing_files = glob.glob("*.csv")
+    
+    if today in existing_files:
+        return False
+    else:
+        return True
+    
+def download_data():
+    response = requests.get(url)
+    with open(f"data/{datetime.now().strftime('%Y-%m-%d')}.csv", "wb") as file:
+        file.write(response.content)
+    
+    return True
+
+
+if __name__ == "__main__":
+
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    if check_date(today):
+        download_data()
+        print(f"Downloaded file for {today}")
+    else:
+        print(f"Already downloaded file for {today}")
